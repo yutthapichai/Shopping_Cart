69e4d4bb10077895d5439193e497ddf5d714cfab
Yutthapichai <cancel_go@hotmail.com>
2018-08-23 19:43:21 +0700

    Category views and make templates

--- perfectcusion/settings.py
+++ perfectcusion/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'perfectcusion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'shop','templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shop.context_processors.menu_links',
             ],
         },
     },
@@ -119,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfieles')
-STATICFIELES_DIRS = (
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
 MEDIA_URL = '/media/'
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Category,Product
 # Create your views here.
 def index(request):
     text_var = 'this is my first jungo app web page.'
     return HttpResponse(text_var)
+
+def allProdCat(request, c_slug=None):
+    c_page = None
+    products = None
+    if c_slug != None:
+        c_page = get_object_or_404(Category,slug=c_slug)
+        products = Product.objects.filter(category=c_page,available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'shop/category.html',{'category':c_page,'products':products})
