d48896f78dd0331876dd3e875663a224c007a0c3
Yutthapichai <cancel_go@hotmail.com>
2018-09-01 22:01:02 +0700

    Paginator

--- shop/views.py
+++ shop/views.py
@@ -10,12 +10,21 @@ def index(request):
 
 def allProdCat(request, c_slug=None):
     c_page = None
-    products = None
+    products_list = None
     if c_slug != None:
         c_page = get_object_or_404(Category,slug=c_slug)
-        products = Product.objects.filter(category=c_page,available=True)
+        products_list = Product.objects.filter(category=c_page,available=True)
     else:
-        products = Product.objects.all().filter(available=True)
+        products_list = Product.objects.all().filter(available=True)
+        paginator = Paginator(products_list, 6)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
     return render(request, 'shop/category.html',{'category':c_page,'products':products})
 
 def ProdCatDatail( request, c_slug, product_slug):
