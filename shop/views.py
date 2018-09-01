19c17381094ed87015d3e05f3ac104792a1f0273
Yutthapichai <cancel_go@hotmail.com>
2018-09-01 21:16:26 +0700

    Add bootstrap4 and awesome5

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Category,Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+
 # Create your views here.
 def index(request):
     text_var = 'this is my first jungo app web page.'
